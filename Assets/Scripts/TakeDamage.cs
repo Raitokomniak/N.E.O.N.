210dc3ad7ea29d036d1890219f33c6381aa7c844
Teemu Tuhkanen <t4tute00@students.oamk.fi>
2017-04-20 10:49:57 +0300

    New more complex way to do silent takedown
    Note: Before we can get UI stuff, you need to monitor the Console.
    After pressing x you have 2 seconds time to react:
    Console reading -1 is left stick directed to left and 1 is left stick
    directed to right.

--- Assets/Scripts/TakeDamage.cs
+++ Assets/Scripts/TakeDamage.cs
@@ -5,44 +5,95 @@ using UnityEngine;
 public class TakeDamage : MonoBehaviour {
 
     // Use this for initialization
+    
     GameControllerScript gScript;
     GameObject player;
+    EnemyPatrollingMovement enemyMov;
+    public float takeDownTime = 2f;
+    float timer;
+    bool takedownStarted;
+    int direction;
+    
     void Awake () {
         gScript = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameControllerScript>();
         player = GameObject.FindGameObjectWithTag("Player");
+        enemyMov = GetComponentInParent<EnemyPatrollingMovement>();
+        timer = 0;
+        direction = 0;
+        takedownStarted = false;
 	}
-
+   
     // Update is called once per frame
     private void OnTriggerStay2D(Collider2D col)
     {
         if (col.gameObject == player)
         {
             //UI tip for the player that silent takedown is enabled
-            int dir = GetComponentInParent<EnemyPatrollingMovement>().facingRight() ? 1 : -1;
+            int dir = enemyMov.facingRight() ? 1 : -1;
             Vector2 directionToTarget = transform.position - player.transform.position;
             float angle = Vector2.Angle(transform.right * dir, directionToTarget);
             if (angle < 90)
             {
+
                 performSilentTakedown();
             }
-
+            if (takedownStarted)
+            {
+                
+                switchOffSystem();
+            }
         }
     }
 
     void performSilentTakedown()
     {
-        if (Input.GetButtonDown("melee"))
+        if (Input.GetButtonDown("melee")&&!takedownStarted)
         {
-            //something fancy here
+            timer = 0;
             player.GetComponent<PlayerMovement>().setPerformAction(true);
-            GetComponentInParent<EnemyPatrollingMovement>().silentKill(true);
-            die();
-            player.GetComponent<PlayerMovement>().setPerformAction(false);
+            enemyMov.silentKill(true);
+            float mark = Random.Range(-2, 1);
+            Debug.Log("MARK " + mark);
+            direction = (mark < 0) ? -1 : 1;
+            takedownStarted = true;
         }
     }
 
     void die()
     {
         gScript.killGuard(this.transform.parent.gameObject);
+        player.GetComponent<PlayerMovement>().setPerformAction(false);
     }
+
+    void switchOffSystem()
+    {
+        Debug.Log(direction);
+        int stickDir = Mathf.RoundToInt(Input.GetAxisRaw("Horizontal"));
+        timer += Time.deltaTime;
+        if (timer < takeDownTime)
+        {
+            if (stickDir == direction)
+            {
+                die();
+                Debug.Log(direction + " " + stickDir);
+            }
+            else if (stickDir == direction * -1)
+            {
+                gScript.setAlertState(true);
+                die();
+                Debug.Log(direction + " " + stickDir);
+            }
+            
+        }
+        else
+        {
+            enemyMov.silentKill(false);
+            enemyMov.playerIsHeard(player.transform.position);
+            takedownStarted = false;
+            player.GetComponent<PlayerMovement>().setPerformAction(false);
+        }
+
+    }
+
+
 }
