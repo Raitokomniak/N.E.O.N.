7c0bde6ec0c2bff564b3cae36951e16ee2632857
Teemu Tuhkanen <t4tute00@students.oamk.fi>
2017-04-23 16:47:44 +0300

    bhhbju

---
+++ Assets/TestRoomScript.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestRoomScript : MonoBehaviour {
+
+    // Use this for initialization
+    public GameObject[] testTubes;
+    List<Light> testTubeLights;
+
+	void Awake () {
+        testTubeLights = new List<Light>();
+        foreach (GameObject tube in testTubes)
+        {
+            testTubeLights.Add(tube.GetComponent<Light>());
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/TestTubeScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestTubeScript : MonoBehaviour {
+
+    // Use this for initialization
+    Light tubeLight;
+    GameObject player;
+
+    void Awake () {
+        tubeLight = this.transform.parent.gameObject.GetComponentInChildren<Light>();
+        player = GameObject.FindGameObjectWithTag("Player");
+        tubeLight.enabled = false;
+	}
+
+    // Update is called once per frame
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == player)
+        {
+            tubeLight.enabled = true;
+        }
+    }
+}
