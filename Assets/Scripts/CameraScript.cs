ed018a4cf591b7a19c32a9604a3ae6b72b618a3b
Teemu Tuhkanen <t4tute00@students.oamk.fi>
2017-05-02 15:03:52 +0300

    hups meni testiversio

--- Assets/Scripts/CameraScript.cs
+++ Assets/Scripts/CameraScript.cs
@@ -27,7 +27,7 @@ public class CameraScript : MonoBehaviour {
         if (!gScript.isDead()&&!usedFromOutside)
         {
            Vector3 targetPos = new Vector3(player.transform.position.x, player.transform.position.y, z);
-           this.transform.position = Vector3.Lerp(this.transform.position, targetPos, smoothing * Time.unscaledDeltaTime);
+           this.transform.position = Vector3.Lerp(this.transform.position, targetPos, smoothing * Time.deltaTime);
         }
     }
 
@@ -36,7 +36,7 @@ public class CameraScript : MonoBehaviour {
         if (!usedFromOutside)
         {
             float zValue = getDistance(z);
-            this.transform.position = Vector3.Lerp(this.transform.position, new Vector3(this.transform.position.x, this.transform.position.y, zValue), 1.5f * Time.unscaledDeltaTime);
+            this.transform.position = Vector3.Lerp(this.transform.position, new Vector3(this.transform.position.x, this.transform.position.y, zValue), 1.5f * Time.deltaTime);
         }
     }
 
