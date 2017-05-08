5f6864a53b442a645a2c102204a6f1dad589ca8b
Jani Kärkkäinen <karkkainen91@gmail.com>
2017-05-08 13:35:04 +0300

    Enginehum

---
+++ Assets/Scripts/EngineHum.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EngineHum : MonoBehaviour {
+    string engineHum = "event:/Environmental sounds/Engine hum";
+    FMOD.Studio.EventInstance engineSound;
+    FMOD.ATTRIBUTES_3D Attributes;
+    // Use this for initialization
+    void Awake ()
+    {
+        engineSound = FMODUnity.RuntimeManager.CreateInstance(engineHum);
+        Vector3 position = transform.position;
+        Attributes = FMODUnity.RuntimeUtils.To3DAttributes(position);
+        engineSound.set3DAttributes(Attributes);
+        engineSound.start();
+    }
+
+    ~EngineHum()
+    {
+        engineSound.stop(FMOD.Studio.STOP_MODE.IMMEDIATE);
+    }
+}
