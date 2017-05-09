3e171c6e449e262d8ae81198d858cbcbdb7ed4bb
Teemu Tuhkanen <t4tute00@students.oamk.fi>
2017-05-09 20:44:15 +0300

    fasfa

--- Assets/SensesIndicator.cs
+++ Assets/SensesIndicator.cs
@@ -21,23 +21,6 @@ public class SensesIndicator : MonoBehaviour {
     void Update()
     {
         handleSprites();
-
-       /* if (Time.timeScale < 0.2f&&!gotHit)
-        {
-            foreach (SpriteRenderer sense in senses)
-            {
-                sense.color = Vector4.Lerp(sense.color, new Vector4(1f, 1f, 1f, 0.5f - Time.timeScale), 0.4f * Time.fixedUnscaledDeltaTime);
-            }
-
-        }
-        else
-        {
-            foreach (SpriteRenderer sense in senses)
-            {
-                sense.color = Vector4.Lerp(sense.color, new Vector4(1f, 1f, 1f, 0), 6 * Time.fixedUnscaledDeltaTime);
-            }
-        }
-        oldTimeScale = Time.timeScale;*/
     }
 
     void handleSprites()
@@ -45,7 +28,6 @@ public class SensesIndicator : MonoBehaviour {
         if (Time.timeScale < 0.2f && !gotHit)
         {
             Vector4 sightColor = gScript.allGuardsAlerted() ? new Vector4(1f, 1f, 1f, 0.5f - Time.timeScale) : new Vector4(0.196f, 1f, 0.196f, 0.5f - Time.timeScale);
-         //   sight.color = sightColor;
             outerCircle.color = Vector4.Lerp(outerCircle.color, new Vector4(1f, 1f, 1f, 0.5f - Time.timeScale), 0.6f * Time.fixedUnscaledDeltaTime);
             innerCircle.color = Vector4.Lerp(innerCircle.color, new Vector4(1f, 1f, 1f, 0.5f - Time.timeScale), 0.4f * Time.fixedUnscaledDeltaTime);
             coreCircle.color = Vector4.Lerp(coreCircle.color, new Vector4(1f, 1f, 1f, 0.5f - Time.timeScale), 0.2f * Time.fixedUnscaledDeltaTime);
--- Assets/TachyIndicator.cs
+++ Assets/TachyIndicator.cs
@@ -8,18 +8,12 @@ public class TachyIndicator : MonoBehaviour {
     public CanvasRenderer[] hexagons;
     void Awake()
     {
-        /* hexagons = new SpriteRenderer[GameObject.FindGameObjectsWithTag("Hexagon").Length];
-         for (int i = 0; i < GameObject.FindGameObjectsWithTag("Hexagon").Length; i++)
-         {
-             hexagons[i] = GameObject.FindGameObjectsWithTag("Hexagon")[i].GetComponent<SpriteRenderer>();
-         }*/
         for (int i = 0; i < hexagons.Length; i++)
         {
             hexagons[i].SetAlpha(0);
             hexagons[i].SetColor(new Color(0.925f, 0.2f, 0.816f));
         }
     }
-	// Update is called once per frame
 	void Update () {
         handleHexagons();
 	}
@@ -27,25 +21,16 @@ public class TachyIndicator : MonoBehaviour {
     void handleHexagons()
     {
        
-        float a = Time.timeScale < 0.2f ? 0.8f - Time.timeScale : 0f;
+        float a = Time.timeScale < 0.2f ? 1 - Time.timeScale : 0f;
+        
         for (int i = 0; i < hexagons.Length; i++)
         {
             if (i == 0)
             {
-                //hexagons[i].color = Vector4.Lerp(hexagons[i].color, new Vector4(1f, 1f, 1f, a), Time.fixedUnscaledDeltaTime);
-               // hexagons[i].SetColor(Vector4.Lerp(hexagons[i].GetColor(), new Vector4(1f, 1f, 1f, a), Time.fixedUnscaledDeltaTime));
                 hexagons[i].SetAlpha(Mathf.Lerp(hexagons[i].GetAlpha(), a, Time.fixedUnscaledDeltaTime));
-            }
-            else if (i == hexagons.Length)
-            {
-                //hexagons[i].color = Vector4.Lerp(hexagons[i].color, new Vector4(1f, 1f, 1f, a), 0.1f * Time.fixedUnscaledDeltaTime);
-              //  hexagons[i].SetColor(Vector4.Lerp(hexagons[i].GetColor(), new Vector4(1f, 1f, 1f, a), 0.1f * Time.fixedUnscaledDeltaTime));
-                hexagons[i].SetAlpha(Mathf.Lerp(hexagons[i].GetAlpha(), a, 0.5f * Time.fixedUnscaledDeltaTime));
             }
             else
             {
-                // hexagons[i].color = Vector4.Lerp(hexagons[i].color, new Vector4(1f, 1f, 1f, a), i*0.1f * Time.fixedUnscaledDeltaTime);
-              //  hexagons[i].SetColor(Vector4.Lerp(hexagons[i].GetColor(), new Vector4(1f, 1f, 1f, a), i *0.1f * Time.fixedUnscaledDeltaTime));
                 hexagons[i].SetAlpha(Mathf.Lerp(hexagons[i].GetAlpha(), a,i*0.5f* Time.fixedUnscaledDeltaTime));
             }
         }
