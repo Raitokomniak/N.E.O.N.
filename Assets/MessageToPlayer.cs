14ed9ab45b0ccb0bb852a9477bcca65fee83ff82
Teemu Tuhkanen <t4tute00@students.oamk.fi>
2017-05-17 15:02:52 +0300

    messaging system

--- Assets/MessageToPlayer.cs
+++ Assets/MessageToPlayer.cs
@@ -6,7 +6,8 @@ public class MessageToPlayer : MonoBehaviour {
 
     // Use this for initialization
     BubbleScript bubble;
-    public string message;
+    public string[] messages;
+    public float textShowTime = 5;
     bool flag;
 	void Awake () {
         bubble = GameObject.FindGameObjectWithTag("GameController").GetComponent<BubbleScript>();
@@ -18,8 +19,17 @@ public class MessageToPlayer : MonoBehaviour {
     {
 	    if (col.gameObject.CompareTag("Player")&&flag)
         {
-            bubble.setText(message, 5f);
+            StartCoroutine(writeText());
             flag = false;
         }
 	}
+
+    IEnumerator writeText()
+    {
+        foreach (string message in messages)
+        {
+            bubble.setText(message, textShowTime);
+            yield return new WaitForSecondsRealtime(textShowTime);
+        }
+    }
 }
