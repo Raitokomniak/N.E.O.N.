d19a5ba661675bb0e1a9604ccea63b93de20b45f
Jani Kärkkäinen <karkkainen91@gmail.com>
2017-05-19 23:14:18 +0300

    humming source back, gizmo savefile, flickering lights and collider fix on gizmo room

---
+++ Assets/flickerScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flickerScript : MonoBehaviour {
+    public Light source;
+    float rand;
+    public float max;
+    public float min;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        rand = Random.Range(min, max);
+        source.intensity = rand;
+	}
+}
